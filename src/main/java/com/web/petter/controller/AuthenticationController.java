c1a6d2f7930d1d2f373c9f5cbd733fab7788f531
Usernow <usernow.82@gmail.com>
2021-11-26 18:25:38 -0500

    Controller Authentication

--- src/main/java/com/web/petter/controller/AuthenticationController.java
+++ src/main/java/com/web/petter/controller/AuthenticationController.java
@@ -1,6 +1,5 @@
 package com.web.petter.controller;
 
-import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.web.petter.model.Customer;
 
 @RestController
 @CrossOrigin(origins="*")
