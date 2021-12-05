4a9ef54e6e56737c3db6addba1b994a5c53d1fbe
Leidy Sanchez <leidy.maria2407@gmail.com>
2021-12-05 16:39:57 -0500

    Ajustes reportes

--- src/main/java/com/web/petter/controller/Reports.java
+++ src/main/java/com/web/petter/controller/Reports.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,32 +31,27 @@ public class Reports {
 	    private CustomerServiceAPI  customerserviceapi;
 
 	
-	@GetMapping("/customer/sales")
-    public List<Salereport> getcustomersalereport(){
+	@GetMapping("{ciudad}/customer/sales")
+    public List<Salereport> getcustomersalereport(@PathVariable String ciudad){
 		Map<Integer, Salereport> map = new HashMap<Integer, Salereport>();
 		List<Sale> ventas= saleServiceAPI.getAll();
 		for (int i = 0; i< ventas.size();i++) {
 			Sale venta =ventas.get(i);
-			if (!map .containsKey(venta.getCustomerId())) {
+			
+			if(!venta.getCity().equals(ciudad))
+				continue;
+			
+			if (!map.containsKey(venta.getCustomerId())) {
 				Customer cliente= customerserviceapi.get(venta.getCustomerId());
 				Salereport sale = new Salereport();
 				sale.setCustomerId(venta.getCustomerId());
 				sale.setCustomerName(cliente.getCustomerName());
 				sale.setTotalSale(0);
 				map.put(venta.getCustomerId(),  sale);
-				
-				
 			}
 			Salereport clienteventa=map.get(venta.getCustomerId());
 			clienteventa.setTotalSale(clienteventa.getTotalSale()+venta.getTotalSale());
-			
-			
 		}
 		return new ArrayList<Salereport>(map.values());
-		
-				
-		
     }
-
-
 }
