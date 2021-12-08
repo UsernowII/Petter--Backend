64d39c3aaba5f055c661954cecdac93e31948cf0
Usernow <usernow.82@gmail.com>
2021-12-08 14:58:56 -0500

    To main

--- src/main/java/com/web/petter/controller/Reports.java
+++ src/main/java/com/web/petter/controller/Reports.java
@@ -8,15 +8,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.petter.model.Customer;
-import com.web.petter.model.Pet;
 import com.web.petter.model.Sale;
-import com.web.petter.model.Salereport;
-import com.web.petter.service.api.ConsolidatedServiceAPI;
+import com.web.petter.model.SaleReport;
 import com.web.petter.service.api.CustomerServiceAPI;
 import com.web.petter.service.api.SaleServiceApi;
 
@@ -32,23 +29,23 @@ public class Reports {
 
 	
 	@GetMapping("customer/sales")
-    public List<Salereport> getcustomersalereport(){
-		Map<Integer, Salereport> map = new HashMap<Integer, Salereport>();
+    public List<SaleReport> getcustomersalereport(){
+		Map<Integer, SaleReport> map = new HashMap<Integer, SaleReport>();
 		List<Sale> ventas= saleServiceAPI.getAll();
 		for (int i = 0; i< ventas.size();i++) {
 			Sale venta =ventas.get(i);
 			
 			if (!map.containsKey(venta.getCustomerId())) {
 				Customer cliente= customerserviceapi.get(venta.getCustomerId());
-				Salereport sale = new Salereport();
+				SaleReport sale = new SaleReport();
 				sale.setCustomerId(venta.getCustomerId());
 				sale.setCustomerName(cliente.getCustomerName());
 				sale.setTotalSale(0);
 				map.put(venta.getCustomerId(),  sale);
 			}
-			Salereport clienteVenta =map.get(venta.getCustomerId());
+			SaleReport clienteVenta =map.get(venta.getCustomerId());
 			clienteVenta.setTotalSale(clienteVenta.getTotalSale()+venta.getTotalSale());
 		}
-		return new ArrayList<Salereport>(map.values());
+		return new ArrayList<SaleReport>(map.values());
     }
 }
--- src/main/java/com/web/petter/model/SaleReport.java
+++ src/main/java/com/web/petter/model/SaleReport.java
@@ -3,16 +3,13 @@ package com.web.petter.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
 
 
 @Getter
 @Setter
 @ToString
-public class Salereport {
+public class SaleReport {
 	
 	private int customerId;
 	private String customerName;
